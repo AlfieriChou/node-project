454707c46d18331e45fe7850e07cf7ab9c18da6c
AlfieriChou <alfierichou@gmail.com>
2018-10-19 20:47:04 +0800

    add express

--- NodeCronJob/index.js
+++ NodeCronJob/index.js
@@ -1,6 +1,13 @@
+const express = require('express')
 const CronJon = require('cron').CronJob
+
+const app = express()
+
 new CronJon('1 * * * * *', () => {
   const date = new Date()
   console.log('------->', date.getSeconds())
   console.log('------->', 'what？')
 }, null, true, 'Asia/Shanghai')
+
+app.listen(3000)
+console.log(`listening on port 3000`)
